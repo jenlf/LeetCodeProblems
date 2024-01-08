75be197861f8df16ae6c9c1a89f22282d41e101a
jenlf <fritz.jenniferlynn@gmail.com>
2024-01-08 10:28:23 -0500

    submitted successful #1431 Kids With the Greatest Number of Candies

---
+++ LeetCode1431.js
@@ -0,0 +1,28 @@
+/*There are n kids with candies. You are given an integer array candies, where each candies[i] represents the number of candies the ith kid has, and an integer extraCandies, denoting the number of extra candies that you have.
+
+Return a boolean array result of length n, where result[i] is true if, after giving the ith kid all the extraCandies, they will have the greatest number of candies among all the kids, or false otherwise.
+
+Note that multiple kids can have the greatest number of candies.*/
+
+/** 
+ * @param {number[]} candies
+ * @param {number} extraCandies
+ * @return {boolean[]}
+ */
+var kidsWithCandies = function(candies, extraCandies) {
+    //find max in candies
+    let maxCandies = Math.max(...candies); //... tells it to expand array 
+    let isGreatest = false;
+    let result = [];
+    //check if each kids candies + extra >= max candies
+    candies.forEach((candy) => {
+        if(candy + extraCandies >= maxCandies) {
+            isGreatest = true;
+        }
+        else {
+            isGreatest = false;
+        }
+        result.push(isGreatest);
+    });
+    return result;
+};
